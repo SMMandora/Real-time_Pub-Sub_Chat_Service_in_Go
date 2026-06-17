534eef5e62040de59b03f9258b95985d19816da2
Shubham Mandora <shubhammandora6@gmail.com>
2026-06-17 00:52:48 -0400

    Wire main entrypoint with graceful shutdown

--- cmd/gateway/main.go
+++ cmd/gateway/main.go
@@ -1,3 +1,57 @@
 package main
 
-func main() {}
+import (
+	"context"
+	"errors"
+	"log/slog"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/SMMandora/Real-time_Pub-Sub_Chat_Service_in_Go/internal/gateway"
+)
+
+func main() {
+	log := slog.New(slog.NewJSONHandler(os.Stdout, nil))
+
+	addr := os.Getenv("ADDR")
+	if addr == "" {
+		addr = ":8080"
+	}
+	webDir := os.Getenv("WEB_DIR")
+	if webDir == "" {
+		webDir = "web"
+	}
+
+	hub := gateway.NewHub()
+	srv := gateway.NewServer(hub, log, webDir)
+	httpServer := &http.Server{Addr: addr, Handler: srv.Router()}
+
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
+
+	go func() {
+		log.Info("gateway listening", "addr", addr)
+		if err := httpServer.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			log.Error("server error", "err", err)
+			stop()
+		}
+	}()
+
+	<-ctx.Done()
+	log.Info("shutdown initiated")
+
+	// Stop routing new traffic, then drain.
+	srv.SetDraining(true)
+
+	shutdownCtx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	hub.CloseAll("server shutting down")
+	if err := httpServer.Shutdown(shutdownCtx); err != nil {
+		log.Error("graceful shutdown failed", "err", err)
+	}
+	log.Info("shutdown complete")
+}
